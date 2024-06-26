0901c839dcc50a77793c45267898836baa9eaa65
JS.Park <kocat53@naver.com>
2024-06-27 00:43:57 +0900

    add : vue 및 일렉트론 설치

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Ticketing System</h1>
+  <button @click="takeTicket">Take Ticket</button>
+  <p>Current Ticket: {{ currentTicket }}</p>
+  <button @click="callNextTicket">Next Ticket</button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
